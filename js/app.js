e61302c321a2f13fbc74af3b90cc45695c212814
vowy <e.b.parsons@gmail.com>
2018-07-05 20:53:20 -0500

    changed win condition, took off lifemeter feature

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 
 let enemyCollision = false;
+
 // Randm Integer Inclusive function, https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random//
 function getRandom(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
 }
+
 
 // Enemies our player must avoid
 class Enemy {
@@ -64,19 +66,15 @@ class Hero {
     this.sprite = 'images/char-boy.png';
   }
   update () {
-    this.speed = 20;
+    this.speed = 30;
     if (enemyCollision === true) {
-      this.x = 200;
-      this.y = 400;
-      this.life --;
+      alert('You Lose :(')
+      location.reload();
       return enemyCollision = false;
     }
+    //win condition
    if (this.y < 0) {
-     alert('YOU WON!!! POP A BENNY')
-   }
-   if (this.life === 0) {
-     alert('You Lose :(')
-     location.reload();
+     alert('YOU WON!!!')
    }
   }
 
